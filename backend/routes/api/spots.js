10f0d883b984dff676048652181d871b31fcc062
aechon <tony.hronowski@gmail.com>
2024-09-23 17:58:54 -0400

    get all spots passing

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,27 +1,41 @@
 const express = require('express');
-const { Spot } = require('../../db/models');
+const { Spot, SpotImage, Review } = require('../../db/models');
+const { json } = require('sequelize');
 
 const router = express.Router();
 
 router.get (`/`, async (req, res) => {
-    try {
-        const spots = await Spot.findAll();
-        res.json(spots)
-    } catch (error) {
+    const spots = await Spot.findAll({
+        include: [
+            {
+                model: SpotImage
+            },
+            {
+                model: Review
+            }
+        ]
+    });
+    
+    const spotsResponse = [];
+    spots.forEach(spot => {
+        jsonSpot = spot.toJSON();
+        jsonSpot.rating = jsonSpot.Reviews.reduce((sum, review) => { 
+            return review.stars + sum
+        }, 0) / jsonSpot.Reviews.length;
         
-    }
+        const preview = jsonSpot.SpotImages.find(image => image.preview === true);
+        if (preview) jsonSpot.preview = preview.url;
+        else jsonSpot.preview = null;
+
+        delete jsonSpot.SpotImages;
+        delete jsonSpot.Reviews;
+
+        spotsResponse.push(jsonSpot);
+
+    })
+
+    res.json(spotsResponse);
 });
-
-// router.get(`/:id`, async (req, res) => {
-
-// });
-
-
-
-
-
-
-
 
 
 module.exports = router;
