6773fc910eea3a97789e81afa3b242606475af80
wisam2882 <wisam_khaleefa@hotmail.com>
2024-10-14 17:08:35 -0700

    stuff

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,21 +1,29 @@
-// backend/routes/index.js
 const express = require('express');
 const router = express.Router();
-
-// backend/routes/index.js
-// ...
 const apiRouter = require('./api');
+const path = require('path');
 
+// Use the API router for all /api routes
 router.use('/api', apiRouter);
-// ...
 
-router.get("/api/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken(); // Generate a new CSRF token
-  res.cookie("XSRF-TOKEN", csrfToken); // Set the cookie with the token
-  res.status(200).json({
-    'XSRF-Token': csrfToken // Send the token in the response
+// Serve React build files in production
+if (process.env.NODE_ENV === 'production') {
+  // Serve the frontend's index.html file at the root route
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken()); // Set the XSRF-TOKEN cookie
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
   });
-});
 
-// Export the router
-module.exports = router;
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve(__dirname, '../../frontend/dist')));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken()); // Set the XSRF-TOKEN cookie
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,22 @@ import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
 
+
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+  // window.sessionActions = sessionActions;
+}
 if (process.env.NODE_ENV !== 'production') {
   window.store = store;
 }
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
